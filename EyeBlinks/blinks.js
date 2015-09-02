9e7270755b3ae420a8ed7e93961877f0ffa4d31a
Chris Parnin <chris.parnin@gmail.com>
2015-09-02 12:28:34 -0400

    Create blinks.js

---
+++ EyeBlinks/blinks.js
@@ -0,0 +1,49 @@
+var s = require('net').Socket();
+s.bufferSize = 4096;
+s.connect(4242, '127.0.0.1',
+function()
+{
+	console.log("Connected");
+	
+
+
+	//s.write('<SET ID="ENABLE_SEND_CURSOR" STATE="1" />\r\n');
+	//s.write('<SET ID="ENABLE_SEND_TIME" STATE="1" />\r\n');
+
+	//s.write('<SET ID="ENABLE_SEND_POG_FIX" STATE="1" />\r\n');
+	//s.write('<SET ID="ENABLE_SEND_DATA" STATE="1" />\r\n');
+
+	//s.write('<SET ID="CALIBRATE_SHOW" STATE="1" />\r\n');
+	//s.write('<SET ID="CALIBRATE_START" STATE="1" />\r\n');
+
+	//s.write('<SET ID="ENABLE_SEND_COUNTER" STATE="1" />\r\n');
+	//s.write('<SET ID="ENABLE_SEND_POG_FIX" STATE="1" />\r\n');
+	//s.write('<SET ID="ENABLE_SEND_USER_DATA" STATE="1" />\r\n');
+
+	//s.write('<SET ID="CALIBRATE_SHOW" STATE="1" />\r\n');
+	//s.write('<SET ID="CALIBRATE_START" STATE="1" />\r\n');
+	//s.write('<SET ID="TRACKER_DISPLAY" STATE="1" />\r\n');
+
+    s.write('<SET ID="ENABLE_SEND_PUPIL_LEFT" STATE="1" />\r\n');
+	//s.write('<SET ID="ENABLE_SEND_EYE_LEFT" STATE="1" />\r\n');
+
+	s.write('<SET ID="ENABLE_SEND_DATA" STATE="1" />\r\n');
+	//s.write('<GET ID="ENABLE_SEND_DATA"/>\r\n');
+
+});
+
+s.on('data', function(d){
+	console.log(s.bytesRead);
+    console.log(d.toString());
+
+});
+
+s.on('error', function(error){
+    console.log(error());
+});
+
+
+s.on('close', function () 
+{
+	console.log("closed");
+});
